1da8f6f2105932e308df1c90ed4c0c2cc68dae03
Matteo Vit <matteo.vit@starwaredesign.com>
2018-12-16 16:45:30 +0000

    Still trying to fix the vivado env issue (try 6).

--- scripts/build_fpga.sh
+++ scripts/build_fpga.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-su vivado
+#su vivado
 cat ~/.bashrc && source ~/.bashrc && env && echo $PATH 
 vivado -mode batch -source recreate_prj.tcl 
 vivado -mode batch -source build_bitstream.tcl
