2d5f8bd8bac2a0075514fa2a166f4aa64b66dc11
Matteo Vit <matteo.vit@starwaredesign.com>
2018-12-16 15:57:56 +0000

    Experiment testing the change of user in Jenkins

--- scripts/build_fpga.sh
+++ scripts/build_fpga.sh
@@ -1,2 +1,3 @@
 #!/bin/bash 
-su - vivado -c 'vivado -mode batch -source recreate_prj.tcl && vivado -mode batch -source build_bitstream.tcl'
+su - vivado -c 'whoami'
+#'vivado -mode batch -source recreate_prj.tcl && vivado -mode batch -source build_bitstream.tcl'
