3ccd6abb0aec322cf07785a4ded316766c1b7a33
Matteo Vit <matteo.vit@starwaredesign.com>
2018-12-16 18:30:19 +0000

    Cleanup FPGA build script.

--- scripts/build_fpga.sh
+++ scripts/build_fpga.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
-#su vivado -c '\
-#source ~/.profile \
-#env && \
-#pwd && \
-#vivado -mode batch -source recreate_prj.tcl && \
-#vivado -mode batch -source build_bitstream.tcl \
-#'
 source /home/vivado/.profile 
-env
-pwd
 cd scripts
 vivado -mode batch -source recreate_prj.tcl 
 vivado -mode batch -source build_bitstream.tcl 
