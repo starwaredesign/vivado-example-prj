66ed374859ac88ff834d1dcf7dcb6c560bfd85f4
Matteo Vit <matteo.vit@starwaredesign.com>
2018-12-16 15:51:49 +0000

    Run the FPGA build as user vivado

--- scripts/build_fpga.sh
+++ scripts/build_fpga.sh
@@ -1,3 +1,2 @@
 #!/bin/bash 
-vivado -mode batch -source recreate_prj.tcl
-vivado -mode batch -source build_bitstream.tcl
+su - vivado -c 'vivado -mode batch -source recreate_prj.tcl && vivado -mode batch -source build_bitstream.tcl'
