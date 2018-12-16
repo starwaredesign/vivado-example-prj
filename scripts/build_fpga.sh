ef9650164e17d834630ac26d3e2bb4f427b0b6b2
Matteo Vit <matteo.vit@starwaredesign.com>
2018-12-16 18:21:44 +0000

    Fixing path issues with Jenkins (try 3).

--- scripts/build_fpga.sh
+++ scripts/build_fpga.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
-su vivado -c '\
-source ~/.profile \
-env && \
-pwd && \
-vivado -mode batch -source recreate_prj.tcl && \
-vivado -mode batch -source build_bitstream.tcl \
-'
+#su vivado -c '\
+#source ~/.profile \
+#env && \
+#pwd && \
+#vivado -mode batch -source recreate_prj.tcl && \
+#vivado -mode batch -source build_bitstream.tcl \
+#'
+source /home/vivado/.profile 
+env
+pwd
+vivado -mode batch -source recreate_prj.tcl 
+vivado -mode batch -source build_bitstream.tcl 
+
  
