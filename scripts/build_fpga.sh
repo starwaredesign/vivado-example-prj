83b1121c644c72c8fc31c753b5cb77fc586198d2
Matteo Vit <matteo.vit@starwaredesign.com>
2018-12-16 16:06:45 +0000

    Add debug info for fixing the vivado user env

--- scripts/build_fpga.sh
+++ scripts/build_fpga.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-su - vivado -c 'source ~/.bashrc && vivado -mode batch -source recreate_prj.tcl && vivado -mode batch -source build_bitstream.tcl'
+su - vivado -c 'cat ~./bashrc && source ~/.bashrc && echo $PATH && vivado -mode batch -source recreate_prj.tcl && vivado -mode batch -source build_bitstream.tcl'
