2d7d4e77b2e7041133ead65f73f6d9f30c789803
Matteo Vit <matteo.vit@starwaredesign.com>
2019-03-25 22:02:29 +0000

    Add resource usage graph.

--- scripts/build_bitstream.tcl
+++ scripts/build_bitstream.tcl
@@ -3,3 +3,6 @@ set project_file [glob -directory vivado *.xpr]
 open_project ${project_file}
 launch_runs impl_1 -to_step write_bitstream -jobs ${ncpu}
 wait_on_run impl_1
+namespace import ::tclapp::xilinx::designutils::report_failfast
+report_failfast -csv -transpose -no_header -file utilisation.csv
+
