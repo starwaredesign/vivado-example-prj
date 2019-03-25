2616f14ec21b73245d7e3710a114e724c4bb7472
Matteo Vit <matteo.vit@starwaredesign.com>
2019-03-25 23:25:52 +0000

    Fix typo in build_bitstream.tcl

--- scripts/build_bitstream.tcl
+++ scripts/build_bitstream.tcl
@@ -6,7 +6,7 @@ launch_runs impl_1 -to_step write_bitstream -jobs ${ncpu}
 wait_on_run impl_1
 namespace import ::tclapp::xilinx::designutils::report_failfast
 open_run [current_run -implementation -quiet]
-report_failfast -csv -transpose -no_header -file ${utilisation_fil}
+report_failfast -csv -transpose -no_header -file ${utilisation_file}
 exec sed /^#/d -i ${utilisation_file}
 exec sed /^"Description"/d -i ${utilisation_file}
 close_design
