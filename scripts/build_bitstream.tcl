68ead65ec37bbe634f37192efa4d40887ab2855b
Matteo Vit <matteo.vit@starwaredesign.com>
2019-03-25 22:26:38 +0000

    Fix build_bitstream.tcl utilisation csv generation.

--- scripts/build_bitstream.tcl
+++ scripts/build_bitstream.tcl
@@ -4,5 +4,6 @@ open_project ${project_file}
 launch_runs impl_1 -to_step write_bitstream -jobs ${ncpu}
 wait_on_run impl_1
 namespace import ::tclapp::xilinx::designutils::report_failfast
+open_run [current_run -implementation -quiet]
 report_failfast -csv -transpose -no_header -file utilisation.csv
-
+close_design
