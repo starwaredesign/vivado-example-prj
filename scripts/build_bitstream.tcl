999b4ac04435d6ab5652a94d6b0d064bcdd376ca
Matteo Vit <matteo.vit@starwaredesign.com>
2019-04-03 23:25:38 +0100

    try reduce csv mods

--- scripts/build_bitstream.tcl
+++ scripts/build_bitstream.tcl
@@ -7,8 +7,8 @@ wait_on_run impl_1
 namespace import ::tclapp::xilinx::designutils::report_failfast
 open_run [current_run -implementation -quiet]
 report_failfast -csv -transpose -no_header -file ${utilisation_file}
-exec sed /^#/d -i ${utilisation_file}
-exec sed /^"Description"/d -i ${utilisation_file}
-exec sed /^$/d -i ${utilisation_file}
+#exec sed /^#/d -i ${utilisation_file}
+#exec sed /^"Description"/d -i ${utilisation_file}
+#exec sed /^$/d -i ${utilisation_file}
 exec sed s/\"//g -i ${utilisation_file}
 close_design
